20425be95557a2202eeeece3d6fc11be73a2b550
josdem <joseluis.delacruz@gmail.com>
2019-07-27 09:42:22 -0400

    Adding person model

---
+++ src/main/java/com/jos/dem/spring/webflux/webclient/model/Person.java
@@ -0,0 +1,15 @@
+package com.jos.dem.spring.webflux.webclient.service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+
+  private String nickname;
+  private String email;
+
+}
