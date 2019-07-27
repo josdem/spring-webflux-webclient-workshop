a87ddd9d6cd8c62e1b785a712b47f45cf22c3fa5
josdem <joseluis.delacruz@gmail.com>
2019-07-27 09:55:20 -0400

    Adding persons with CommandRunner :)

--- src/main/java/com/jos/dem/spring/webflux/webclient/DemoApplication.java
+++ src/main/java/com/jos/dem/spring/webflux/webclient/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.jos.dem.spring.webflux.webclient;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.jos.dem.spring.webflux.webclient.model.Person;
+import com.jos.dem.spring.webflux.webclient.repository.PersonRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,6 +19,22 @@ public class DemoApplication {
   @Bean
   WebClient webClient() {
     return WebClient.create("http://localhost:8080");
+  }
+
+  @Bean
+  CommandLineRunner start(PersonRepository personRepository){
+    return args -> {
+
+      Flux.just(
+          new Person("josdem", "joseluis.delacruz@gmail.com"),
+          new Person("tgrip", "tgrip@email.com"),
+          new Person("edzero", "edzero@email.com"),
+          new Person("siedrix", "siedrix@email.com"),
+          new Person("mkheck", "mkheck@losheckler.com"))
+        .flatMap(personRepository::save)
+        .subscribe(person -> log.info("person: {}", person));
+
+    };
   }
 
 }
