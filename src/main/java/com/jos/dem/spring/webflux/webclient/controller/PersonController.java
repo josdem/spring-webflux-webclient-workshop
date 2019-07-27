db38884b96ffc951860da2271c85caa405a886ab
josdem <joseluis.delacruz@gmail.com>
2019-07-27 10:40:50 -0400

    Fixing compilation errors

--- src/main/java/com/jos/dem/spring/webflux/webclient/controller/PersonController.java
+++ src/main/java/com/jos/dem/spring/webflux/webclient/controller/PersonController.java
@@ -1,12 +1,14 @@
 package com.jos.dem.spring.webflux.webclient;
 
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.jos.dem.spring.webflux.webclient.model.Person;
 import com.jos.dem.spring.webflux.webclient.repository.PersonRepository;
 
 import org.slf4j.Logger;
@@ -22,7 +24,7 @@ public class PersonController {
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @GetMapping("/")
-  public Flux<Persons> findAll(){
+  public Flux<Person> findAll(){
     log.info("Getting persons");
     return personRepository.getAll();
   }
