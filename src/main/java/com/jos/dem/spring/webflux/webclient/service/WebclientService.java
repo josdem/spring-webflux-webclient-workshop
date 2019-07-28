1e1c2d18df7e9a0f9f5bb419b34767d70fa65785
josdem <joseluis.delacruz@gmail.com>
2019-07-28 13:47:22 -0400

    Changing to return a flux of persons

--- src/main/java/com/jos/dem/spring/webflux/webclient/service/WebclientService.java
+++ src/main/java/com/jos/dem/spring/webflux/webclient/service/WebclientService.java
@@ -1,11 +1,15 @@
 package com.jos.dem.spring.webflux.webclient.service;
 
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
 import org.springframework.http.HttpHeaders;
+
+import com.jos.dem.spring.webflux.webclient.model.Person;
 
 public interface WebclientService {
 
-  Mono<String> getGreetings();
+  Flux<Person> findAll();
   Mono<HttpHeaders> getHeaders();
 
 }
--- src/main/java/com/jos/dem/spring/webflux/webclient/service/impl/WebclientServiceImpl.java
+++ src/main/java/com/jos/dem/spring/webflux/webclient/service/impl/WebclientServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jos.dem.spring.webflux.webclient.service.impl;
 
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,17 +10,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.jos.dem.spring.webflux.webclient.service.WebclientService;
 
+import com.jos.dem.spring.webflux.webclient.model.Person;
+
 @Service
 public class WebclientServiceImpl implements WebclientService {
 
   @Autowired
   private WebClient webClient;
 
-  public Mono<String> getGreetings(){
+  public Flux<Person> findAll(){
     return webClient.get()
-      .uri("/")
+      .uri("/persons/")
       .retrieve()
-    .bodyToMono(String.class);
+    .bodyToFlux(Person.class);
   }
 
   public Mono<HttpHeaders> getHeaders(){
