a7e1e278b85bdff72b124d8122b7f0a0a29eb47d
josdem <joseluis.delacruz@gmail.com>
2019-12-13 07:46:27 -0500

    Deleting unused code

--- src/main/java/com/jos/dem/spring/webflux/webclient/DemoApplication.java
+++ src/main/java/com/jos/dem/spring/webflux/webclient/DemoApplication.java
@@ -1,18 +1,15 @@
 package com.jos.dem.spring.webflux.webclient;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import com.jos.dem.spring.webflux.webclient.model.Person;
 import com.jos.dem.spring.webflux.webclient.repository.PersonRepository;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -21,11 +18,6 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
-  }
-
-  @Bean
-  WebClient webClient() {
-    return WebClient.create("http://localhost:8080");
   }
 
   @Bean
