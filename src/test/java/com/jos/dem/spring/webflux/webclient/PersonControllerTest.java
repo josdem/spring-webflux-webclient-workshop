e3967a4c4cdd330b0a2dc98d0f5613896e70337d
josdem <joseluis.delacruz@gmail.com>
2021-09-20 16:09:02 -0500

    #2 Adding test info to the test cases

--- src/test/java/com/jos/dem/spring/webflux/webclient/PersonControllerTest.java
+++ src/test/java/com/jos/dem/spring/webflux/webclient/PersonControllerTest.java
@@ -5,15 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import java.util.Date;
 import org.springframework.web.reactive.function.BodyInserters;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -27,32 +24,37 @@ class PersonControllerTest {
 
   @Test
   @DisplayName("Should get all of persons")
-  void shouldGetAllPersons() {
-    log.info("Running: Should get all persons at {}", new Date());
+  void shouldGetAllPersons(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
 
-    webTestClient.get().uri("/persons/")
-            .exchange()
-            .expectStatus().isOk()
-            .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
-            .expectBodyList(Person.class)
-            .value(persons -> persons.size(), equalTo(5))
-            .value(persons -> persons.contains(new Person("josdem","joseluis.delacruz@gmail.com")))
-            .value(persons -> persons.contains(new Person("tgrip", "tgrip@email.com")))
-            .value(persons -> persons.contains(new Person("edzero", "edzero@email.com")))
-            .value(persons -> persons.contains(new Person("siedrix", "siedrix@email.com")))
-            .value(persons -> persons.contains(new Person("mkheck", "mkheck@losheckler.com")));
+    webTestClient
+        .get()
+        .uri("/persons/")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .expectBodyList(Person.class)
+        .value(persons -> persons.size(), equalTo(5))
+        .value(persons -> persons.contains(new Person("josdem", "joseluis.delacruz@gmail.com")))
+        .value(persons -> persons.contains(new Person("tgrip", "tgrip@email.com")))
+        .value(persons -> persons.contains(new Person("edzero", "edzero@email.com")))
+        .value(persons -> persons.contains(new Person("siedrix", "siedrix@email.com")))
+        .value(persons -> persons.contains(new Person("mkheck", "mkheck@losheckler.com")));
   }
 
   @Test
   @DisplayName("Should create a person")
-  void shouldCreatePerson() {
-    log.info("Running: Should create a person at {}", new Date());
+  void shouldCreatePerson(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
 
-    webTestClient.post()
+    webTestClient
+        .post()
         .uri("/persons/")
-        .body(BodyInserters.fromObject(new Person("starbuxman","josh@email.com")))
+        .body(BodyInserters.fromObject(new Person("starbuxman", "josh@email.com")))
         .exchange()
-        .expectStatus().isOk();
+        .expectStatus()
+        .isOk();
   }
-
 }
--- src/test/java/com/jos/dem/spring/webflux/webclient/WebfluxControllerTest.java
+++ src/test/java/com/jos/dem/spring/webflux/webclient/WebfluxControllerTest.java
@@ -1,47 +1,51 @@
 package com.jos.dem.spring.webflux.webclient;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.util.Date;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class WebfluxControllerTest {
 
-  private Logger log = LoggerFactory.getLogger(this.getClass());
-
-  @Autowired
-  private WebTestClient webTestClient;
+  private final WebTestClient webTestClient;
 
   @Test
   @DisplayName("Should validate message with Hamcrest")
-  public void shouldValidateMessageWithHamcrest() throws Exception {
-    log.info("Running: Should validate message with Hamcrest at {}", new Date());
+  public void shouldValidateMessageWithHamcrest(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
 
-    webTestClient.get().uri("/")
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody(String.class).isEqualTo("Hello World!");
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(String.class)
+        .isEqualTo("Hello World!");
   }
 
   @Test
   @DisplayName("Should validate message with Junit Jupiter")
-  public void shouldValidatetMessageWithJunitJupiter() throws Exception {
-    log.info("Running: Should validate message with Junit Jupiter at {}", new Date());
+  public void shouldValidatetMessageWithJunitJupiter(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
 
-    webTestClient.get().uri("/")
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody(String.class)
-            .value(message -> assertEquals("Hello World!", message));
+    webTestClient
+        .get()
+        .uri("/")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(String.class)
+        .value(message -> assertEquals("Hello World!", message));
   }
-
 }
